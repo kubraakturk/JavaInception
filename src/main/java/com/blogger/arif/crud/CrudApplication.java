501af9ba4dac879bf9a9e17dbbee0034b1f50fec
Kübra Aktürk <kubraakturk1@gmail.com>
2022-10-27 21:12:25 +0300

    x

--- src/main/java/com/blogger/arif/crud/CrudApplication.java
+++ src/main/java/com/blogger/arif/crud/CrudApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//deneme
 public class CrudApplication {
 
     public static void main(String[] args) {
