af7babe2405db7bfc5e7ef2d09d3c13b1695bc9d
Kübra Aktürk <kubraakturk1@gmail.com>
2022-10-27 23:11:49 +0300

    x

--- src/main/java/com/blogger/arif/crud/repositories/DeveloperRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blogger.arif.crud.repositories;
-
-import com.blogger.arif.crud.models.DeveloperModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeveloperRepository extends JpaRepository<DeveloperModel,Long> {
-}
